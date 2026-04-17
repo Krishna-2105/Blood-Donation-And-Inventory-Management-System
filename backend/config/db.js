c0df3326d0afaba9f3d7000e94f920437007b737
NadigeniRaju <nadigeninadigeniraju@gmail.com>
2026-04-17 13:28:44 +0530

    Update db.js file
    Increased connection and queue limits for the database.

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,8 +6,8 @@ const db = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10, // Max concurrent connections
-  queueLimit: 0        // 0 = No limit on waiting requests
+  connectionLimit: 50, // Max concurrent connections
+  queueLimit: 50 // 0 = No limit on waiting requests
 });
 
 module.exports = db; 
